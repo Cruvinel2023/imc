021691511e88cbdc93842b23d63fae4a20f594b1
Cruvinel2023 <ranierycruvinel@gmail.com>
2024-01-06 11:31:55 -0300

    atualização do projeto imc

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-var peso;
- var altura;
- var imc;
+
 
 
  function calcularImc() {
-    peso= parseFloat(document.getElementById("weight").value);
-    altura= parseFloat (document.getElementById("height").value);
-    imc = peso/(altura*altura);
-    document.getElementById("result").textContent = ("Seu IMC é: " + imc.toFixed(2));
+    peso= parseFloat(document.getElementById("weight").value.replace(',', '.'));
+    altura= parseFloat (document.getElementById("height").value.replace(',', '.'));
+    imc = peso /(altura*altura);
+   document.getElementById("result").value = ("Seu IMC é: " + imc.toFixed(2));
+   determinarCategoria(imc);
+  
+ }
+
+ function determinarCategoria(imc) {
+  var categoria;
+  if (imc < 18.5) {
+    categoria = "Abaixo do peso";
+  } else if (imc < 25) {
+    categoria = "Peso Normal";
+  } else if (imc < 30) { 
+    categoria = "Sobrepeso";
+  } else  {
+    categoria = "Obesidade";
+  }
+  document.getElementById("category").value = categoria;
  }
  
+   
